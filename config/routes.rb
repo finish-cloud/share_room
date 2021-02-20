d18572901bc3d69eae27676ffa723594d1ea2c92
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-02-20 18:32:18 +0900

    roomコントローラとビューファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'rooms/new'
+  get 'rooms/create'
+  get 'rooms/edit'
+  get 'rooms/update'
+  get 'rooms/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
