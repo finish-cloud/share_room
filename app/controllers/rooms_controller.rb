4bad276a07e27bd4d0f4f8cdf1a510d4d76c3cac
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-02-20 18:41:10 +0900

    投稿一覧・新規投稿ページへのリンクを追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,14 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.all
   end
 
   def show
+    @room = Room.find(params[:id])
   end
 
   def new
+    @room = Room.new
   end
 
   def create
